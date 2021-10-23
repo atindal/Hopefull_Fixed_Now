16629bd3dfc88ae57140e3258bfaf5a57c5d1da4
Vyzeon <jws2927@gmail.com>
2021-10-23 15:58:14 -0400

    First Code Build
    Starting point for code, only has a button that changes text

---
+++ VandalStats/app/src/main/java/com/example/vandalstats/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.vandalstats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button pushButton = findViewById(R.id.changebutton);
+        TextView hw = findViewById(R.id.changeable);
+
+        pushButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hw.setText("Button has been pressed.");
+            }
+        });
+    }
+}
